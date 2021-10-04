f2bb3d7f409dffb1c8714c8c2741aaff1c153c34
adarsh jaiswal <adarshjaiswal05@gmail.com>
2021-10-04 17:43:36 +0530

    commit

---
+++ contracts/MyToken.sol
@@ -0,0 +1,13 @@
+MyToken.sol
+
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("nanotech token", "NTT") {
+        _mint(msg.sender, 10**26);
+    }
+}
